22fc43759c7e13d7907ca8b3bf34f8e66101c878
Xing Voong <xingvoong@gmail.com>
2022-02-10 10:51:16 -0800

    implement solution for carPooling

--- meeting_rooms_and_intervals/car_pooling/carPooling.py
+++ meeting_rooms_and_intervals/car_pooling/carPooling.py
@@ -1,4 +1,4 @@
-'''
+"""
 There is a car with capacity empty seats.  The vehicle only drives east (i.e, it cannot turn around and drive west).
 
 You are given the integer capacity and an array trips where trips[i] = [numPassengers_i, from_i, to_i] indicates that the i_th trip has numPassengers_i passengers and the locations to pick them up and drop them off are from_i to_i respectively.  The locations are given as the number of kilometers due east from the car's init location.
@@ -13,6 +13,32 @@ example 2:
 input: trips = [[2, 1, 5], [3, 3, 7]] capacity = 5
 output: true
 
-'''
+"""
 
-def carPooling(trips):
+
+def carPooling(trips, capacity):
+    timestamps = []
+    current_capacity = 0
+    for trip in trips:
+        timestamps.append([trip[1], trip[0]])
+        timestamps.append([trip[2], -trip[0]])
+
+    # sort base on picking up time
+    timestamps.sort()
+
+    # go through the timstamps to check capacity:
+    for timestamp in timestamps:
+        current_capacity += timestamp[1]
+        if current_capacity > capacity:
+            return False
+
+    return True
+
+
+trips = [[2, 1, 5], [3, 3, 7]]
+capacity = 4
+
+trips2 = [[2, 1, 5], [3, 3, 7]]
+capacity = 5
+
+print(carPooling(trips, capacity))
