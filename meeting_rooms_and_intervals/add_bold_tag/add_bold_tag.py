5a7011634350f2a95140fca930266e80de0d37e1
Xing Voong <xingvoong@gmail.com>
2022-02-12 19:26:31 -0800

    init add bold tag in string

---
+++ meeting_rooms_and_intervals/add_bold_tag/add_bold_tag.py
@@ -0,0 +1,10 @@
+'''
+You are given a string s and an array of strings words.  You should add a closed pair of bold tag <b> and </b> to wrap the substrings in s that exist in words.
+- if 2 such substrings overlap, you should wrap them together with only one pair of closed bold-tag.
+- if 2 substrings wrapped by bold tags are consecutive, you should combine them.
+
+Return s after adding the bold tags
+
+
+'''
+
